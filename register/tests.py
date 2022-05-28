6d3cb81588402de72c22192e962259a823b0deaf
ryumada <rizukiryumada@gmail.com>
2022-05-28 14:47:31 +0700

    fixed the clutters
    + added newLine in evry code files that doesn't have
    - removed git merge conflict clutters

--- register/tests.py
+++ register/tests.py
@@ -13,5 +13,3 @@ class TestRegister(APITestCase):
         # self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
         # self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    
